45a197866403452789ee873d87c224a70b83fc61
keita <ba0503naky@gmail.com>
2020-07-23 17:04:04 +0900

    add view action

--- src/components/view-text.js
+++ src/components/view-text.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 import styled from 'styled-components'
+
 const Main = styled.div`
   width: 100%;
   height: 100%;
@@ -10,59 +11,118 @@ const Main = styled.div`
 const View = styled.div`
   margin: 0 auto;
   text-align: center;
+ 
+  /*以下遅延の指定*/
 `
-const Row = 17;
-const Col = 10;
+const isNumber = function (value) {
+  return Number.isFinite(value);
+};
+const Int = (num) => isNumber(num) ? Math.floor(num) : 0;
+const baseText = 'あいうえ'
+const zero = (val) => ('00' + val).slice(-2);
+
+/* 
+# テキストエリアの縦横幅を取得
+# 上記をそれぞれ標準ボックスサイズで割る
+# それぞれの整数値を文字数とする
+# ※当面、最小数は無視 ▶ フォントサイズ・ボックスサイズは固定
+# おいおい、ボックスサイズ・フォントサイズをstore化することで
+# サイズの変更が可能
+*/
+const BaseWidth = 36;
+const BaseHeight = 20;
+const FontSize = 16;
+const Rows = (height) => Int(height / BaseHeight);
+const Cols = (width) => Int(width / BaseWidth);
 const Char = styled.span`
   display: inline-block;
   position:relative;
   text-align: center;
-  width: ${props => props.width};
-  height: ${props => props.height};
-  font-size: 16px;
-  border: 1px solid #bbb;
+  width: ${BaseWidth + 'px'};
+  height: ${BaseHeight + 'px'};
+  font-size: ${FontSize + 'px'};
+  /* box-sizing: content-box;  */
+  /* border: 1px solid #ccc; */
 `
 const Str = styled.span`
   position:absolute;
-  top:0;
+  top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
+  &.fadein {
+    opacity: 0;
+    animation: fadein ${props => props.speed}s ease forwards;
+    animation-delay: ${props => props.delay}s;
+  }
+  @keyframes fadein {
+      100% {  opacity: 1;}
+  }
 `
 const Tr = styled.div`
-  height: ${props => props.height};
+  height: ${BaseHeight + 'px'};
 `
-
 const ViewText = () => {
   const textRef = useRef(null);
-  const [size, setSize] = useState({ width: null, height: null, boxWidth: null, boxHeight: null })
+  const [size, setSize] = useState({ width: null, height: null })
+  const [text, setText] = useState({})
+  const setId = (rowIndex, cols, colIndex) => {
+    const colRev = (cols.length - 1) - colIndex
+    return zero(colRev) + '-' + zero(rowIndex);
+  }
+  const optimizeText = ({ height, width }) => {
+    console.log(Rows(height))
+    console.log(Cols(width))
+    const maxRow = Rows[height]
+    const maxCol = Cols[width]
+    let [row, col] = [0, 0]
+    const text = {}
+    Array.from(baseText).map(char => {
+      text[`${zero(row)}-${zero(col)}`] = char;
+      return 0;
+    })
+    return text;
+  }
 
   useEffect(() => {
-    const resize = function handleResize(size) {
+    const resize = async function handleResize() {
       const tag = textRef.current;
-      setSize({
+      await setSize({
         height: tag.offsetHeight,
         width: tag.offsetWidth,
-        boxHeight: tag.offsetHeight / Row + 'px',
-        boxWidth: tag.offsetWidth / Col + 'px',
       })
-      console.log(size)
+      setText(optimizeText({
+        height: tag.offsetHeight,
+        width: tag.offsetWidth,
+      }))
     }
-    resize();
+    if (!size.width) resize();
+    // console.log(size)
+    // console.log(Rows(size.height))
+    // console.log(Cols(size.width))
 
     window.addEventListener('resize', resize)
     return _ => {
       window.removeEventListener('resize', resize)
     }
-  }, [])
+  })
   return (
     <Main ref={textRef}>
       <View>
-        {size.boxWidth && [...Array(Row).keys()].map(row => {
+        {size.width && [...Array(Rows(size.height)).keys()].map((row, rowIndex) => {
           return (
-            <Tr key={row} height={size.boxHeight}>
-              {[...Array(Col).keys()].map(col => {
+            <Tr key={row} >
+              {[...Array(Cols(size.width)).keys()].map((col, colIndex, c) => {
                 return (
-                  <Char key={col} width={size.boxWidth} height={size.boxHeight}>
-                    <Str>○</Str>
+                  <Char key={col} >
+                    <Str
+                      className="fadein"
+                      id={setId(rowIndex, c, colIndex)}
+                      delay={rowIndex * 0.2}
+                      speed={2}
+                    >
+                      {text[setId(rowIndex, c, colIndex)]}
+                    </Str>
                   </Char>
                 )
               })}
